6eb32af13700d92d2a0b089c1899bce004bac4ee
Edward.Safford <edward.safford@noaa.gov>
2022-06-21 16:25:23 +0000

    Ref #7
    Correct lua files.

--- modulefiles/hera.intel.lua
+++ modulefiles/hera.intel.lua
@@ -5,10 +5,12 @@ prepend_path("MODULEPATH", "/scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/mo
 
 local hpc_ver=os.getenv("hpc_ver") or "1.1.0"
 local hpc_intel_ver=os.getenv("hpc_intel_ver") or "18.0.5.274"
+local hpc_impi_ver=os.getenv("hpc_impi_ver") or "2018.0.4"
 local cmake_ver=os.getenv("cmake_ver") or "3.20.1"
 
 load(pathJoin("hpc", hpc_ver))
 load(pathJoin("hpc-intel", hpc_intel_ver))
+load(pathJoin("hpc-impi", hpc_impi_ver))
 load(pathJoin("cmake", cmake_ver))
 
 load("common")
--- modulefiles/s4.lua
+++ modulefiles/s4.lua
@@ -3,12 +3,14 @@ help([[
 
 local hpc_ver=os.getenv("hpc_ver") or "1.1.0"
 local hpc_intel_ver=os.getenv("hpc_intel_ver") or "18.0.4"
+local hpc_impi_ver=os.getenv("hpc_impi_ver") or "18.0.4"
 
 prepend_path("MODULEPATH", "/data/prod/hpc-stack/modulefiles/stack")
 
 load("license_intel/S4")
 load(pathJoin("hpc", hpc_ver))
 load(pathJoin("hpc-intel", hpc_intel_ver))
+load(pathJoin("hpc-impi", hpc_impi_ver))
 
 load("common")
 
