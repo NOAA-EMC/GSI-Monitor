e8cb8122f8c4e333aac2f98030130d73a60805be
edward.safford <edward.safford@noaa.gov>
2022-08-11 14:03:49 -0500

    Ref #14
    update orion-run mod

--- modulefiles/orion-run.lua
+++ modulefiles/orion-run.lua
@@ -6,10 +6,12 @@ prepend_path("MODULEPATH", "/apps/contrib/NCEP/libs/hpc-stack/modulefiles/stack"
 local hpc_ver=os.getenv("hpc_ver") or "1.1.0"
 local hpc_intel_ver=os.getenv("hpc_intel_ver") or "2018.4"
 local grads_ver=os.getenv("grads_ver") or "2.2.1"
+local prod_util_ver=os.getenv("prod_util_ver") or "1.2.2"
 
 load(pathJoin("hpc", hpc_ver))
 load(pathJoin("hpc-intel", hpc_intel_ver))
 load(pathJoin("grads", grads_ver))
+load(pathJoin("prod_util", prod_util_ver))
 
 load("common-run")
 
