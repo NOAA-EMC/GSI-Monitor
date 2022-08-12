866d68b74a7f36dc59d2bc86dccb72d21a08b65b
edward.safford <edward.safford@noaa.gov>
2022-08-12 12:52:14 +0000

    Ref #14
    Finalize lua run mods

--- modulefiles/common-run.lua
+++ modulefiles/common-run.lua
@@ -3,8 +3,6 @@ Load common modules to run GSI Monitoring on all machines
 ]])
 
 local wgrib2_ver=os.getenv("wgrib2_ver") or "2.0.8"
---local prod_util_ver=os.getenv("prod_util_ver") or "1.2.2"
 
 load(pathJoin("wgrib2", wgrib2_ver))
---load(pathJoin("prod_util", prod_util_ver))
 
