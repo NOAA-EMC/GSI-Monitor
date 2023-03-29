88fd5864ae4dd3858ef4fed960ed7c0511c45f70
Edward.Safford <edward.safford@noaa.gov>
2023-03-29 17:14:40 +0000

    Ref #74
    Update wcoss2 modulefiles.

--- modulefiles/wcoss2-run.lua
+++ modulefiles/wcoss2-run.lua
@@ -2,16 +2,16 @@ help([[
 ]])
 
 local intel_ver=os.getenv("intel_ver") or "19.1.3.304"
+local prod_util_ver=os.getenv("prod_util_ver") or "2.0.13"
+local prod_envir_ver=os.getenv("prod_envir_ver") or "2.0.6"
 
 load(pathJoin("intel", intel_ver))
+load(pathJoin("prod_util", prod_util_ver))
+load(pathJoin("prod_envir", prod_envir_ver))
 
 
 prepend_path("MODULEPATH", "/apps/test/lmodules/core/")
 load ("GrADS/2.2.2")
-
-prepend_path("MODULEPATH", "/apps/ops/para/nco/modulefiles/core/")
-load ("prod_util/2.0.13")
-load ("prod_envir/2.0.6")
 
 load("common-run")
 
--- modulefiles/wcoss2.lua
+++ modulefiles/wcoss2.lua
@@ -4,20 +4,15 @@ help([[
 local PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.1.0"
 local intel_ver=os.getenv("intel_ver") or "19.1.3.304"
 local craype_ver=os.getenv("craype_ver") or "2.7.8"
+local cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.7"
 local cmake_ver= os.getenv("cmake_ver") or "3.20.2"
 
 load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
 load(pathJoin("intel", intel_ver))
 load(pathJoin("craype", craype_ver))
+load(pathJoin("cray-mpich", cray_mpich_ver))
 load(pathJoin("cmake", cmake_ver))
 
 load("common")
-unload("ncdiag")
-
-pushenv("HPC_OPT", "/apps/ops/para/libs")
-prepend_path("MODULEPATH", "/apps/ops/para/libs/modulefiles/compiler/intel/19.1.3.304")
-prepend_path("MODULEPATH", "/apps/ops/para/libs/modulefiles/mpi/intel/19.1.3.304/cray-mpich/8.1.7")
-
-load("ncdiag/1.0.0")
 
 whatis("Description: GSI Monitoring environment on WCOSS2")
