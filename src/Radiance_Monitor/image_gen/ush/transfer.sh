70f5d393d232e421c71076ce728faa176e4628a8
edward.safford <edward.safford@noaa.gov>
2023-07-14 17:01:43 +0000

    Ref #92
    Update transfer to work on hera.

--- src/Radiance_Monitor/image_gen/ush/transfer.sh
+++ src/Radiance_Monitor/image_gen/ush/transfer.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
-if [[ ${IMGNDIR} != "/" ]]; then
+if [[ ${TANKimg} != "/" && -d ${TANKimg} ]]; then
+
+   WEBSVR=${WEBSVR}.ncep.noaa.gov
+
+   #----------------------------------------------------------
+   # If the destination directory exists (on the server) then
+   # sync the html there with the $TANKimg directory so we
+   # have a backup copy.  Note the pngs subdirectory is 
+   # skipped -- we only want to update the html and related
+   # site files in $TANKimg, not the files in /pngs.  Also 
+   # note that the use of the --update option means that if a 
+   # file exists in both places the destination file is not 
+   # updated if it's newer.
+   #
+   # Else create the destintation directory on the server.
+   #----------------------------------------------------------
+
+   if ssh ${WEBUSER}@${WEBSVR} "[ -d ${WEBDIR} ]"; then
+      /usr/bin/rsync -ave ssh --exclude 'pngs/' --update \
+         ${WEBUSER}@${WEBSVR}:${WEBDIR}/ ${TANKimg}
+   else
+      ssh ${WEBUSER}@${WEBSVR} "mkdir -p ${WEBDIR}"
+   fi
+
    /usr/bin/rsync -ave ssh --exclude *.ctl.${Z} \
-      --exclude 'horiz' --exclude *.png --delete-during ${IMGNDIR}/ \
-      ${WEBUSER}@${WEBSVR}.ncep.noaa.gov:${WEBDIR}/
+      --exclude 'horiz' --exclude *.png --delete-during --update ${TANKimg}/ \
+      ${WEBUSER}@${WEBSVR}:${WEBDIR}/
+
+else
+   echo "Unable to run rsync, TANKimg has bad/no value of: ${TANKimg}"
 fi
 
 exit
