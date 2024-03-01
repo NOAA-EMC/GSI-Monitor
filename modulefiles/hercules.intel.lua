1340d3f4a5adb87b02ce244e601021a0e8c8b816
David Huber <david.huber@noaa.gov>
2024-03-01 12:31:26 -0600

    Upgrade Hercules to spack-stack v1.6.0; remove perl module
    Use system perl instead.  New Perl modules were installed during the
    last system maintainance period.

--- modulefiles/hercules.intel.lua
+++ modulefiles/hercules.intel.lua
@@ -1,7 +1,7 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.5.1/envs/gsi-addon/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
 
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
 local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
