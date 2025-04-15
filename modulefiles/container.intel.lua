94e57070c082bf653875f901fca0535afb6ddf11
Wei Huang <Wei.Huang@noaa.gov>
2025-04-15 00:59:23 +0000

    for container

--- modulefiles/container.intel.lua
+++ modulefiles/container.intel.lua
@@ -1,11 +1,13 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.8.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/intel-oneapi-mpi/2021.9.0/intel/2021.10.0")
+prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/intel/2021.10.0")
 
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.12.2"
-local cmake_ver=os.getenv("cmake_ver") or "3.27.9"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
+local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
--- ush/module-setup.sh
+++ ush/module-setup.sh
@@ -24,10 +24,11 @@ elif [[ $MACHINE_ID = orion* ]] ; then
 
 elif [[ $MACHINE_ID = container* ]] ; then
     # We are in a container
-    if ( ! eval module help > /dev/null 2>&1 ) ; then
+    # if ( ! eval module help > /dev/null 2>&1 ) ; then
         source /usr/lmod/lmod/init/bash
-    fi
+    # fi
     module purge
+    unset MODULEPATH
 
 elif [[ $MACHINE_ID = hercules* ]] ; then
     # We are on Hercules
