56b599cecf05f5caedbe874a9ea83c277ea13bdb
Taylor Roper <taylor.roper@noaa.gov>
2025-05-05 19:00:16 +0000

    moved updated noaacloud lua files into modulefiles directory

--- modulefiles/noaacloud.intel.lua
+++ modulefiles/noaacloud.intel.lua
@@ -1,15 +1,17 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/apps/modules/modulefiles")
 
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.3.0"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.3.0"
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.10.0"
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 
+load("gnu")
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
-load(pathJoin("cmake", cmake_ver))
+unload("gnu")
 
 load("common")
 
