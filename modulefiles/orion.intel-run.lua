dc8477b4fd0840d8dedfb6667ae33ce18ca496bb
edward.safford <edward.safford@noaa.gov>
2025-06-05 12:53:19 -0500

    Ref #182
    Update orion module files for spack-stack-1.9.1

--- modulefiles/orion.intel-run.lua
+++ modulefiles/orion.intel-run.lua
@@ -2,21 +2,21 @@ help([[
 ]])
 
 -- Spack Stack installation specs
-local ss_dir="/work/noaa/epic/role-epic/spack-stack/orion"
-local ss_ver=os.getenv("stack_ver") or "1.6.0"
-local ss_env=os.getenv("stack_env") or "gsi-addon-env-rocky9"
+prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.1/envs/ue-oneapi-2024.1.0/install/modulefiles/Core")
 
-prepend_path("MODULEPATH", pathJoin(ss_dir, "spack-stack-" .. ss_ver, "envs", ss_env, "install/modulefiles/Core"))
+local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
+local stack_intel_oneapi_mpi_ver=os.getenv("stack_intel_oneapi_mpi_ver") or "2021.13"
 
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
-local grads_ver=os.getenv("grads_ver") or "2.2.1"
+local grads_ver=os.getenv("grads_ver") or "2.2.3"
 local prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
 
-load(pathJoin("stack-intel", stack_intel_ver))
-load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_intel_oneapi_mpiver))
+
 load(pathJoin("grads", grads_ver))
 load(pathJoin("prod_util", prod_util_ver))
+
+setenv("wgrib2_ver", "3.6.0")
 
 load("common-run")
 
