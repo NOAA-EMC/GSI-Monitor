d5380b7a81a51d60afae88e0ea4fb2e074d95d74
Taylor Roper <taylor.roper@noaa.gov>
2025-07-15 08:54:35 -0600

    Update cloud module files (#191)
    * Add gcc version and maintain gnu stack (#189)
    
    * Update to spack-stack 1.9.1 (#189)
    
    * Update to spack-stack 1.9.2 (#189)

--- modulefiles/noaacloud.intel.lua
+++ modulefiles/noaacloud.intel.lua
@@ -1,18 +1,16 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/apps/modules/modulefiles")
 
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.10.0"
-local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+local gcc_ver=os.getenv("gcc_ver") or "13.2.0"
+local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
 
-load("gnu")
-load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("gnu", gcc_ver))
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
-unload("gnu")
-load(pathJoin("cmake", cmake_ver))
 
 load("common")
 
