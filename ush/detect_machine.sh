27ffa4e57a5aca29382ae31d5ab50cdd110b2722
Wei Huang <Wei.Huang@noaa.gov>
2025-07-24 14:23:07 +0000

    use container ss-1.9.1

--- ush/detect_machine.sh
+++ ush/detect_machine.sh
@@ -8,6 +8,12 @@
 #
 # Thank you for your contribution
 
+# Overwrite auto-detect if in container
+if [[ -d /opt/spack-stack && -v SINGULARITY_CONTAINER ]]; then
+  # We are  in a container
+  MACHINE_ID=container
+fi
+
 # If the MACHINE_ID variable is set, skip this script.
 [[ -n ${MACHINE_ID:-} ]] && return
 
@@ -57,7 +63,7 @@ fi
 MACHINE_ID=${MACHINE:-${MACHINE_ID}}
 
 # Overwrite auto-detect if in container
-if [[ -d /opt/spack-stack ]]; then
+if [[ -d /opt/spack-stack && -v SINGULARITY_CONTAINER ]]; then
   # We are  in a container
   MACHINE_ID=container
 fi
