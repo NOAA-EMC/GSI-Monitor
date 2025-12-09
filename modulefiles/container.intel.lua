0de78b2830c127421fb84678b661062d61684e23
Wei Huang <Wei.Huang@noaa.gov>
2025-12-09 15:04:16 +0000

    add container ss192 modulefiles

--- modulefiles/container.intel.lua
+++ modulefiles/container.intel.lua
@@ -1,13 +1,15 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.9.1/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.9.2/envs/unified-env/install/modulefiles/Core")
 
-stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.0"
-stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.0"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+local cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 
 load(pathJoin("stack-oneapi", stack_oneapi_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+load(pathJoin("cmake", cmake_ver))
 
 load("common")
 
