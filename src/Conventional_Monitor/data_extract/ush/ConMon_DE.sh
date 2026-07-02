db10b8d1105995a9d06108208a1e06268ee6f14f
Edward.Safford <edward.safford@noaa.gov>
2026-07-02 19:10:01 +0000

    Ref #216 Increase runtime for DE job.

--- src/Conventional_Monitor/data_extract/ush/ConMon_DE.sh
+++ src/Conventional_Monitor/data_extract/ush/ConMon_DE.sh
@@ -242,7 +242,7 @@ if [ -s $cnvstat  -a -s $pgrbf00 -a -s $pgrbf06 ]; then
 		${HOMEgdas_conmon}/jobs/JGDAS_ATMOS_CONMON
       
       elif [[ $MY_MACHINE = "wcoss2" ]]; then
-        $SUB -V -q $JOB_QUEUE -A $ACCOUNT -o ${logfile} -e ${logfile} -l walltime=45:00 -N ${jobname} \
+        $SUB -V -q $JOB_QUEUE -A $ACCOUNT -o ${logfile} -e ${logfile} -l walltime=2:15:00 -N ${jobname} \
 		-l select=1:mem=8gb ${HOMEgdas_conmon}/jobs/JGDAS_ATMOS_CONMON
       fi
 
