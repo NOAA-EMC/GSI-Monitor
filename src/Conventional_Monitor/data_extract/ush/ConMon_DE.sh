9025c92e74851d36f056f810997e2ba4ca2bc5ad
Edward.Safford <edward.safford@noaa.gov>
2026-07-09 13:02:13 +0000

    Ref #220
    Reduce wall time.

--- src/Conventional_Monitor/data_extract/ush/ConMon_DE.sh
+++ src/Conventional_Monitor/data_extract/ush/ConMon_DE.sh
@@ -231,12 +231,12 @@ if [ -s $cnvstat  -a -s $pgrbf00 -a -s $pgrbf06 ]; then
 		${HOMEgdas_conmon}/jobs/JGDAS_ATMOS_CONMON
      
       elif [[ $MY_MACHINE = "ursa" ]]; then
-         $SUB -A $ACCOUNT --ntasks=1 --time=01:30:00 \
+         $SUB -A $ACCOUNT --ntasks=1 --time=00:30:00 \
 		-J ${jobname} -o $C_LOGDIR/DE.${PDY}.${CYC}.log \
 		${HOMEgdas_conmon}/jobs/JGDAS_ATMOS_CONMON
 
       elif [[ $MY_MACHINE = "wcoss2" ]]; then
-        $SUB -V -q $JOB_QUEUE -A $ACCOUNT -o ${logfile} -e ${logfile} -l walltime=2:15:00 -N ${jobname} \
+        $SUB -V -q $JOB_QUEUE -A $ACCOUNT -o ${logfile} -e ${logfile} -l walltime=0:30:00 -N ${jobname} \
 		-l select=1:mem=8gb ${HOMEgdas_conmon}/jobs/JGDAS_ATMOS_CONMON
       fi
 
